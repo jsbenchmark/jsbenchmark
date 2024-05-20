7bb87e1ec93ca692f908a047ce4d0a19e27ed1d2
Pascal Bürkle <25956751+pabueco@users.noreply.github.com>
2024-05-20 17:09:47 +0200

    improve contrast of result bar background

--- components/Results.vue
+++ components/Results.vue
@@ -48,7 +48,7 @@ const colors = computed(() => {
           {{ stateByTest[test.id]?.result?.opsPerSecond?.toLocaleString() || '?' }}
         </div>
       </div>
-      <div class="relative rounded-[0.375em] bg-gray-800/50">
+      <div class="relative rounded-[0.375em] bg-gray-800">
         <div
           class="rounded-[0.375em] h-[2.75em] transition-all duration-500 striped"
           :class="{
